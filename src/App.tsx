8d00b2872c5f5aee29530d01c8f920697f074c7a
Joav Abraham-Beermann <joav.abraham.beermann@gmail.com>
2019-12-31 09:11:25 +1300

    disable buttons on fetch

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import './App.css';
 import IPerson from './IPerson'
-//import Person from './Person'
 import PersonComponent from './PersonComponent'
 import { observer } from 'mobx-react';
 import State from './State'
@@ -12,19 +11,7 @@ import { PushSpinner } from "react-spinners-kit";
 
 @observer
 class App extends React.Component<{ appState: State }, {}> {
-  /*
-    constructor(props: any) {
-      super(props);
-      
-      const person: IPerson = { name: "jarjar" }
-      this.state = {
-        person: person,
-        imgURL: "https://en.wikipedia.org/wiki/C-3PO#/media/File:C-3PO_droid.png",
-        personNumber:1
-      }
-    }
-  */
-
+ 
   incrementFetchPerson() {
     this.props.appState.incrementPersonCount();
     this.fetchPerson();
@@ -38,46 +25,30 @@ class App extends React.Component<{ appState: State }, {}> {
     fetch(`https://swapi.co/api/people/` + this.props.appState.personNumber + '/')
       .then(res => res.json())
       .then(res => {
-        let b: IPerson = res as IPerson;
-        //console.log(b);
-        
+        let p: IPerson = res as IPerson;
+        this.props.appState.person = p;
         //try get the main image of a star wars character from the wikipedia article using its api, include copywrited images
-        let res2 = encodeURI("https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=original&titles=https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=original&pilicense=any&titles=" + b.name)
+        let res2 = encodeURI("https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=original&titles=https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=original&pilicense=any&titles=" + p.name)
         fetch(res2)
-          .then(res => res.json())
-          .then(response => {
+          .then(res2 => res2.json())
+          .then(res2 => {
             try {
               this.props.appState.isLoading = false;
-              this.props.appState.imgURL = response.query.pages[0].original.source
-              this.props.appState.person = b;
+              this.props.appState.imgURL = res2.query.pages[0].original.source             
             }
             catch (e) {
               this.props.appState.imgURL = "https://upload.wikimedia.org/wikipedia/commons/6/6c/No_image_3x4.svg"
             }
-          }).catch((error) => {
+          }).catch((e) => {
             this.props.appState.isLoading = false;
-            this.props.appState.person = b;
+            console.log(e);
           });
-        /*let res2 = encodeURI("https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?autoCorrect=false&pageNumber=1&pageSize=10&q="+b.name+"&safeSearch=false");
-        fetch(res2, {
-          "method": "GET",
-          "headers": {
-            "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
-            "x-rapidapi-key": "1311b8f2d5mshbd9b7ae2edcde0fp1ae31ajsne2443347e794"
-          }
-        })
-        .then(res => res.json())
-        .then(response => {
-          console.log(response);
-        })
-        .catch(err => {
-          console.log(err);
-        });*/
-      }).catch(err => {
+      }).catch(e => {
         this.props.appState.isLoading = false;
-        console.log(err);
+        console.log(e);
       })
   }
+
   componentDidMount() {
     this.fetchPerson();
   }
@@ -95,10 +66,12 @@ class App extends React.Component<{ appState: State }, {}> {
           </div>
           <div>
             <span className={decrementClass}>
-              {<button onClick={this.decrementFetchPerson.bind(this)}>Prev</button>}
+              {<button onClick={this.decrementFetchPerson.bind(this)} 
+                disabled={this.props.appState.isLoading}>Prev</button>}
             </span>
             <span>
-              <button onClick={this.incrementFetchPerson.bind(this)}>Next</button>
+              <button onClick={this.incrementFetchPerson.bind(this)} 
+              disabled={this.props.appState.isLoading}>Next</button>
             </span>
           </div>
         </header>
@@ -106,6 +79,4 @@ class App extends React.Component<{ appState: State }, {}> {
     );
   }
 }
-// gender={this.props.appState.person.gender} 
-//this.handleClick.bind(this)
 export default App;
