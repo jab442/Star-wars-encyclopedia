966f20cd9c7f060b38986be5a67e2fdf2054235a
Joav Abraham-Beermann <joav.abraham.beermann@gmail.com>
2019-12-30 20:18:24 +1300

    fixed css a bit , added catches to fetch

--- src/App.tsx
+++ src/App.tsx
@@ -29,31 +29,35 @@ class App extends React.Component<{ appState: State }, {}> {
     this.props.appState.incrementPersonCount();
     this.fetchPerson();
   }
-  decrementFetchPerson(){
+  decrementFetchPerson() {
     this.props.appState.decrementPersonCount();
     this.fetchPerson();
   }
   fetchPerson() {
-    this.props.appState.isLoading=true;
+    this.props.appState.isLoading = true;
     fetch(`https://swapi.co/api/people/` + this.props.appState.personNumber + '/')
       .then(res => res.json())
       .then(res => {
         let b: IPerson = res as IPerson;
-        console.log(b);
-        this.props.appState.person = b;
+        //console.log(b);
+        
         //try get the main image of a star wars character from the wikipedia article using its api, include copywrited images
         let res2 = encodeURI("https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=original&titles=https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=original&pilicense=any&titles=" + b.name)
         fetch(res2)
           .then(res => res.json())
           .then(response => {
-            try{
-              this.props.appState.isLoading=false;
+            try {
+              this.props.appState.isLoading = false;
               this.props.appState.imgURL = response.query.pages[0].original.source
+              this.props.appState.person = b;
             }
-            catch(e){
+            catch (e) {
               this.props.appState.imgURL = "https://upload.wikimedia.org/wikipedia/commons/6/6c/No_image_3x4.svg"
             }
-        })
+          }).catch((error) => {
+            this.props.appState.isLoading = false;
+            this.props.appState.person = b;
+          });
         /*let res2 = encodeURI("https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?autoCorrect=false&pageNumber=1&pageSize=10&q="+b.name+"&safeSearch=false");
         fetch(res2, {
           "method": "GET",
@@ -69,26 +73,33 @@ class App extends React.Component<{ appState: State }, {}> {
         .catch(err => {
           console.log(err);
         });*/
+      }).catch(err => {
+        this.props.appState.isLoading = false;
+        console.log(err);
       })
   }
   componentDidMount() {
     this.fetchPerson();
   }
   render() {
-    const decrementClass:string=(this.props.appState.personNumber >=2)?"":"hidden";
+    const decrementClass: string = (this.props.appState.personNumber >= 2) ? "" : "hidden";
     return (
       <div className="App">
         <header className="App-header">
-          <PersonComponent name={this.props.appState.person.name} gender={this.props.appState.person.gender}/>
-          <div>
+          <PersonComponent name={this.props.appState.person.name} gender={this.props.appState.person.gender} />
+          <div id="spinnerDiv">
             <PushSpinner size={30} color="#686769" loading={this.props.appState.isLoading} />
-            <img id="personImage" src={this.props.appState.imgURL} alt={"loading image.."} />
           </div>
-          <div className={decrementClass}>
-            {<button onClick={this.decrementFetchPerson.bind(this)}>Previous</button>}
+          <div id="personImageDiv">
+            <img id="personImage" src={this.props.appState.imgURL} />
           </div>
           <div>
-            <button onClick={this.incrementFetchPerson.bind(this)}>Next</button>
+            <span className={decrementClass}>
+              {<button onClick={this.decrementFetchPerson.bind(this)}>Prev</button>}
+            </span>
+            <span>
+              <button onClick={this.incrementFetchPerson.bind(this)}>Next</button>
+            </span>
           </div>
         </header>
       </div>
