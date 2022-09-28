239199c81158604c402c729d0e61d7bcac02e8d0
joav.abraham.beermann@gmail.com <joav.abraham.beermann@gmail.com>
2022-09-29 09:38:08 +1300

    changed star wars url

--- src/App.tsx
+++ src/App.tsx
@@ -24,24 +24,8 @@ class App extends React.Component<{ appState: State }, {}> {
   }
   fetchPerson() {
     this.props.appState.isLoading = true;
-    /*
-       birth_year: "19BBY"
-      created: "2014-12-09T13:50:51.644000Z"
-      edited: "2014-12-20T21:17:56.891000Z"
-      eye_color: "blue"
-      films: (5) […]
-      gender: "male"
-      hair_color: "blond"
-      height: "172"
-      homeworld: "https://swapi.co/api/planets/1/"
-      mass: "77"
-      name: "Luke Skywalker"
-      skin_color: "fair"
-      species: (1) […]
-      starships: (2) […]
-      url: "https://swapi.co/api/people/1/"
-      vehicles: (2) […]*/
-    fetch(`https://swapi.co/api/people/` + this.props.appState.personNumber + '/')
+    //https://swapi.dev/api/films/1/
+    fetch(`https://swapi.dev/api/people/` + this.props.appState.personNumber + '/')
       .then(res => res.json())
       .then(res => {
         let p: IPerson = res as IPerson;
